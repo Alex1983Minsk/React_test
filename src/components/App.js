dde8b6309b008850e00a3c9df2ab9bc29f00634c
Alex1983Minsk <alex.galenchyk@yandex.ru>
2023-03-16 21:15:44 +0300

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+//
+import Header from "./Header/Header"
+import Footer from "./Footer/Footer"
+import Main from "./Main/Main"
+
+function App() {
+    return (
+        `
+        ${Header()}
+        ${Main()}
+        ${Footer()}
+        
+        `
+    )
+}
+
+export default App
---
+++ src/components/Main/Main.js
@@ -0,0 +1,13 @@
+function Main() {
+    return (
+        `
+        <main class="main conteiner text-center p-5">
+            <h1>Title text</h1>
+        <p>Description text</p>
+            
+        </main>    
+        `
+    )
+}
+
+export default Main
